913026c3b69438a16a62da113f33f7a60a5d8a94
juwon-code <juwoncode@gmail.com>
2025-07-11 19:01:53 +0900

    refactor: 내부 DTO 제거 및 응답 DTO 검증 애노테이션 제거
    응답 DTO에 대한 유효성 검사는 서비스 계층에서 수행하므로. 응답 객체에서 @Valid 관련 설정을 제거했습니다.
    또한, 컴포넌트 계층간 데이터 이동의 역할을 명확히 하기 위해, 내부(Internal) DTO를 제거하고 요청 및 응답 DTO로 분산했습니다.
    
    - 내부 DTO 제거 및 역할 재분배.
    - 응답 DTO Valid 옵션 제거.
    
    Related to: #9

--- src/main/java/com/prgrmsfinal/skypedia/member/dto/MemberRequestDTO.java
+++ src/main/java/com/prgrmsfinal/skypedia/member/dto/MemberRequestDTO.java
@@ -1,23 +1,55 @@
 package com.prgrmsfinal.skypedia.member.dto;
 
+import com.prgrmsfinal.skypedia.global.constant.SocialType;
 import com.prgrmsfinal.skypedia.photo.dto.PhotoRequestDto;
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import lombok.Builder;
 import org.hibernate.validator.constraints.Length;
 
-public sealed interface MemberRequestDto permits MemberRequestDto.ChangeProfile, MemberRequestDto.ChangeRole {
-    record ChangeProfile(
+public sealed interface MemberRequestDto permits MemberRequestDto.SocialInfo, MemberRequestDto.SearchOptions,
+        MemberRequestDto.Modify, MemberRequestDto.ChangeRole {
+    record SocialInfo(
+            @NotNull(message = "소셜 식별 ID는 비워둘 수 없습니다.")
+            String oauthId,
+
+            @NotBlank(message = "성명은 비워둘 수 없습니다.")
+            String name,
+
+            @NotNull(message = "이메일은 비워둘 수 없습니다.")
+            @Email(message = "이메일의 형식에 맞지 않습니다.")
+            String email,
+
+            SocialType socialType
+    ) implements MemberRequestDto {
+        @Builder
+        public SocialInfo {}
+    }
+
+    record SearchOptions(
+            Long memberId,
+
+            String oauthId,
+
+            Boolean removed
+    ) implements MemberRequestDto {
+        @Builder
+        public SearchOptions {}
+    }
+
+    record Modify(
             @Length(min = 2, max = 20, message = "닉네임은 2 ~ 20자 길이여야 합니다.")
             @Pattern(regexp = "^[^\\s]+$", message = "닉네임은 공백을 포함될 수 없습니다.")
             String nickname,
 
             @Valid
-            PhotoRequestDto.Upload profilePhotoData
+            PhotoRequestDto.Upload photoData
     ) implements MemberRequestDto {
         @Builder
-        public ChangeProfile {}
+        public Modify {}
     }
 
     record ChangeRole(
--- src/main/java/com/prgrmsfinal/skypedia/member/dto/MemberResponseDTO.java
+++ src/main/java/com/prgrmsfinal/skypedia/member/dto/MemberResponseDTO.java
@@ -1,92 +1,81 @@
 package com.prgrmsfinal.skypedia.member.dto;
 
-import com.fasterxml.jackson.annotation.JsonInclude;
+import com.prgrmsfinal.skypedia.global.constant.RoleType;
 import com.prgrmsfinal.skypedia.global.constant.SocialType;
-import jakarta.validation.constraints.*;
 import lombok.Builder;
-import org.hibernate.validator.constraints.Length;
-import org.hibernate.validator.constraints.URL;
-import org.hibernate.validator.constraints.UUID;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
-public sealed interface MemberResponseDto permits MemberResponseDto.ReadProfile, MemberResponseDto.SimpleProfile
-        , MemberResponseDto.ChangeProfile, MemberResponseDto.Withdraw, MemberResponseDto.Login {
-    record ReadProfile(
-            @Length(min = 2, max = 20, message = "닉네임은 2 ~ 20자 길이여야 합니다.")
-            @NotBlank(message = "닉네임은 비워둘 수 없습니다.")
-            @Pattern(regexp = "^[^\\s]+$", message = "닉네임은 공백을 포함될 수 없습니다.")
+public sealed interface MemberResponseDto permits MemberResponseDto.Info, MemberResponseDto.SimpleProfile
+        , MemberResponseDto.Profile, MemberResponseDto.SearchProfile, MemberResponseDto.SignIn, MemberResponseDto.Modify
+        , MemberResponseDto.Remove {
+    record Info(
+            Long id,
             String nickname,
-
-            @NotNull(message = "이메일은 비워둘 수 없습니다.")
-            @Email(message = "이메일 형식과 일치하지 않습니다.")
-            String email,
-
-            @NotNull(message = "소셜 계정 형식은 비워둘 수 없습니다.")
-            SocialType socialType,
-
-            @URL(message = "프로필 사진 링크가 올바른 URL 형식이 아닙니다.")
-            String profilePhotoUrl
+            String photoUrl,
+            List<RoleType> roleTypes
     ) implements MemberResponseDto {
         @Builder
-        public ReadProfile {}
+        public Info {}
     }
 
     record SimpleProfile(
-            @Length(min = 2, max = 20, message = "닉네임은 2 ~ 20자 길이여야 합니다.")
-            @NotBlank(message = "닉네임은 비워둘 수 없습니다.")
-            @Pattern(regexp = "^[^\\s]+$", message = "닉네임은 공백을 포함될 수 없습니다.")
             String nickname,
-
-            @URL(message = "프로필 사진 링크가 올바른 URL 형식이 아닙니다.")
-            String profilePhotoUrl
+            String photoUrl
     ) implements MemberResponseDto {
         @Builder
         public SimpleProfile {}
     }
 
-    record ChangeProfile(
-            @Length(min = 2, max = 20, message = "닉네임은 2 ~ 20자 길이여야 합니다.")
-            @Pattern(regexp = "^[^\\s]+$", message = "닉네임은 공백을 포함할 수 없습니다.")
-            @JsonInclude(JsonInclude.Include.NON_NULL)
+    record Profile(
             String nickname,
-
-            @UUID(message = "올바른 UUID 형태가 아닙니다.")
-            @JsonInclude(JsonInclude.Include.NON_NULL)
-            String uuid,
-
-            @URL(message = "프로필 사진 업로드 링크는 URL 형식이어야 합니다.")
-            @JsonInclude(JsonInclude.Include.NON_NULL)
-            String uploadUrl
+            String email,
+            SocialType socialType,
+            String photoUrl,
+            boolean removed
     ) implements MemberResponseDto {
         @Builder
-        public ChangeProfile {}
+        public Profile {}
     }
 
-    record Withdraw(
-            @NotNull(message = "삭제한 날짜는 비워둘 수 없습니다.")
-            LocalDateTime removedAt,
-
-            @NotNull(message = "영구 삭제되는 날짜는 비워둘 수 없습니다.")
-            LocalDateTime willDeletedAt
+    record SearchProfile(
+            Long id,
+            String nickname,
+            String email,
+            String socialType,
+            String photoUrl,
+            List<String> roleTypes,
+            LocalDateTime createdAt,
+            LocalDateTime removedAt
     ) implements MemberResponseDto {
         @Builder
-        public Withdraw {}
+        public SearchProfile {}
     }
 
-    record Login(
-            @Length(min = 2, max = 20, message = "닉네임은 2 ~ 20자 길이여야 합니다.")
-            @NotBlank(message = "닉네임은 비워둘 수 없습니다.")
-            @Pattern(regexp = "^[^\\s]+$", message = "닉네임은 공백을 포함될 수 없습니다.")
+    record SignIn(
             String nickname,
-
-            @URL(message = "프로필 사진 링크가 올바른 URL 형식이 아닙니다.")
-            String profilePhotoUrl,
-
-            @NotNull(message = "인증 토큰은 반드시 발급되어야 합니다.")
+            String photoUrl,
             String accessToken
     ) implements MemberResponseDto {
         @Builder
-        public Login {}
+        public SignIn {}
+    }
+
+    record Modify(
+            String nickname,
+            String uuid,
+            String uploadUrl
+    ) implements MemberResponseDto {
+        @Builder
+        public Modify {}
+    }
+
+    record Remove(
+            LocalDateTime removedAt,
+            LocalDateTime willDeletedAt
+    ) implements MemberResponseDto {
+        @Builder
+        public Remove {}
     }
 }
